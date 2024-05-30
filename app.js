3959d4845ca8f03166fec2c0646bb1b170fb7c39
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-05-30 16:16:07 +0300

    module 2

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
-function add(n1, n2, showResult, phrase) {
-    //JS
-    //   if (typeof n1 !== "number" || typeof n2 !== "number") {
-    //     throw new Error("Incorrect input");
-    //   }
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var num1;
-num1 = 5;
-var num2 = 5.5;
-var printResult = false;
-var resultPhrase = "result is: ";
-add(num1, num2, printResult, resultPhrase);
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+var person = {
+    name: "Artem",
+    age: 23,
+};
+console.log(person.age);
--- app.ts
+++ app.ts
@@ -1,4 +1,10 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
 const person = {
   name: "Artem",
   age: 23,
 };
+
+console.log(person.age);
