084f2af7efa33be4c186c5c71ad43887fe5e04c3
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-05-30 16:18:37 +0300

    module 2

--- app.js
+++ app.js
@@ -7,3 +7,22 @@ var person = {
     age: 23,
 };
 console.log(person.age);
+// NESTED OBJECTS
+// {
+//   id: string;
+//   price: number;
+//   tags: string[];
+//   details: {
+//     title: string;
+//     description: string;
+//   }
+// }
+var product = {
+    id: "abc1",
+    price: 12.99,
+    tags: ["great-offer", "hot-and-new"],
+    details: {
+        title: "Red Carpet",
+        description: "A great carpet - almost brand-new!",
+    },
+};
