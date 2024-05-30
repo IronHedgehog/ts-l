cd1601517c30379c31c61d715c93dd5133c5818a
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-05-30 16:46:53 +0300

    module 2

--- app.js
+++ app.js
@@ -2,15 +2,29 @@
 //   name: string;
 //   age: number;
 // } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; // Tuple - fixed length array
+// } = {
+//   name: "Artem",
+//   age: 23,
+//   hobbies: ["Sport", "Cooking"],
+//   role: [1, "author"],
+// };
+//person.role.push("admin"); // Exception
+// person.role[1] = 10; // Error
+//person.role = [10, "a", "aasd"]; // Error
+var ADMIN = 0;
+var READ_ONLY = 1;
+var AUTHOR = 2;
 var person = {
     name: "Artem",
     age: 23,
     hobbies: ["Sport", "Cooking"],
-    role: [1, "author"],
+    role: ADMIN,
 };
-//person.role.push("admin"); // Exception
-// person.role[1] = 10; // Error
-//person.role = [10, "a", "aasd"]; // Error
 var favoriteActives;
 favoriteActives = ["something"];
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
@@ -38,3 +52,6 @@ var product = {
         description: "A great carpet - almost brand-new!",
     },
 };
+if (person.role === ADMIN) {
+    console.log("is admin");
+}
--- app.ts
+++ app.ts
@@ -2,22 +2,33 @@
 //   name: string;
 //   age: number;
 // } = {
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string]; // Tuple - fixed length array
-} = {
-  name: "Artem",
-  age: 23,
-  hobbies: ["Sport", "Cooking"],
-  role: [1, "author"],
-};
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; // Tuple - fixed length array
+// } = {
+//   name: "Artem",
+//   age: 23,
+//   hobbies: ["Sport", "Cooking"],
+//   role: [1, "author"],
+// };
 
 //person.role.push("admin"); // Exception
 // person.role[1] = 10; // Error
 
 //person.role = [10, "a", "aasd"]; // Error
+
+const ADMIN = 0;
+const READ_ONLY = 1;
+const AUTHOR = 2;
+
+const person = {
+  name: "Artem",
+  age: 23,
+  hobbies: ["Sport", "Cooking"],
+  role: ADMIN,
+};
 
 let favoriteActives: string[];
 favoriteActives = ["something"];
@@ -47,3 +58,7 @@ const product = {
     description: "A great carpet - almost brand-new!",
   },
 };
+
+if (person.role === ADMIN) {
+  console.log("is admin");
+}
