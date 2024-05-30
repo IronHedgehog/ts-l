196a2bee09ce5ecd2c28095198da28da60dde543
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-05-30 15:40:20 +0300

    module 2

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult, phrase) {
+    //JS
+    //   if (typeof n1 !== "number" || typeof n2 !== "number") {
+    //     throw new Error("Incorrect input");
+    //   }
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
 }
 var num1 = 10;
 var num2 = 5.5;
-console.log(add(num1, num2));
+var printResult = false;
+var resultPhrase = "result is: ";
+add(num1, num2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,12 +1,19 @@
-function add(n1: number, n2: number) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   //JS
   //   if (typeof n1 !== "number" || typeof n2 !== "number") {
   //     throw new Error("Incorrect input");
   //   }
-  return n1 + n2;
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
 }
 
 const num1 = 10;
 const num2 = 5.5;
+const printResult = false;
+const resultPhrase = "result is: ";
 
-console.log(add(num1, num2));
+add(num1, num2, printResult, resultPhrase);
