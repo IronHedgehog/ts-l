95cb87d01d8e6d40075c7e05fce6c0fc76ee35f8
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-06-04 14:27:32 +0300

    return types

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
-// union type
-function combine(
-// union type
-// input1: number | string,
-input1, 
-// input2: number | string,
-input2, 
-// Literal type
-resultConversion) {
-    var result;
-    if ((typeof input1 === "number" && typeof input2 === "number") ||
-        resultConversion === "is-number") {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(30, 26, "is-number");
-console.log(combinedAges);
-var combinedAgesStrings = combine("30", "26", "is-number");
-var combinedNames = combine("Max", "Anna", "is-string");
-console.log(combinedNames);
+function printResult(num) {
+    console.log(num);
+}
+printResult(add(20, 10));
