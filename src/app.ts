a4e0fda183eb8736ec9ec063245e3765f3cd34e9
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-06-19 13:56:31 +0300

    public private modifier

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-  name: string;
+  public name: string; // all variables public by default
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,15 +9,31 @@ class Department {
   describe(this: Department) {
     console.log(`Department: ${this.name}`);
   }
+
+  addEmployee(employee: string) {
+    // add validation
+    this.employees.push(employee);
+  }
+
+  printEmployeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const somethingDepartment = new Department("somethingDepartment");
+
+somethingDepartment.addEmployee("Max");
+somethingDepartment.addEmployee("Petro");
+
+// somethingDepartment.employees[2] = "Ann"; ERROR because of employees private field
+
 somethingDepartment.describe();
 
 // const copyOfSomethingDepartment = { describe: somethingDepartment.describe }; ERROR because need Department class signature
-const copyOfSomethingDepartment = {
-  name: "SomethingDepartmentCopy",
-  describe: somethingDepartment.describe,
-};
+// const copyOfSomethingDepartment = {
+//   name: "SomethingDepartmentCopy",
+//   describe: somethingDepartment.describe,
+// };
 
-copyOfSomethingDepartment.describe();
+// copyOfSomethingDepartment.describe();
