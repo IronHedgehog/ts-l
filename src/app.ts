38679aef6360de9f6777f204e69b9b3551adf0d8
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-06-21 12:27:09 +0300

    function generics

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,31 @@
-const names: Array<string> = [""]; // Array<string> ===  string[], arrays of strings
+// const names: Array<string> = [""]; // Array<string> ===  string[], arrays of strings
 
-const promise: Promise<number> = new Promise((res, rej) => {
-  setTimeout(() => {
-    res(10);
-  }, 2000);
-});
+// const promise: Promise<number> = new Promise((res, rej) => {
+//   setTimeout(() => {
+//     res(10);
+//   }, 2000);
+// });
 
-promise.then((res) => {
-  // console.log(res.join()); ERROR because of our generic say result of this promise its number
-  console.log(res.toFixed(2));
-});
+// promise.then((res) => {
+//   // console.log(res.join()); ERROR because of our generic say result of this promise its number
+//   console.log(res.toFixed(2));
+// });
+
+// function mergeObjects(obj1: object, obj2: object) {
+//   return { ...obj1, ...obj2 };
+// }
+// console.log(result.); with type object doesn't work because it can be any object
+
+// if we use generic, generic type compiling when we takes params
+
+function mergeObjects<T, U>(obj1: T, obj2: U) {
+  return { ...obj1, ...obj2 };
+}
+
+// generic behind the scene <{ name: string; hobby: Array<string> },{ age: number }> NO NEED TO WRITE IT (GENERIC DO THIS)
+const result = mergeObjects<
+  { name: string; hobby: Array<string> },
+  { age: number }
+>({ name: "Artem", hobby: ["Sport"] }, { age: 61 });
+
+console.log(result.name);
