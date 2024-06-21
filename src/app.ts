ac1b257522931ae2b2960733f2af63d5c81c7adc
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-06-21 12:39:52 +0300

    generic constraints

--- src/app.ts
+++ src/app.ts
@@ -18,14 +18,17 @@
 
 // if we use generic, generic type compiling when we takes params
 
-function mergeObjects<T, U>(obj1: T, obj2: U) {
+function mergeObjects<T extends object, U extends object>(obj1: T, obj2: U) {
   return { ...obj1, ...obj2 };
 }
 
 // generic behind the scene <{ name: string; hobby: Array<string> },{ age: number }> NO NEED TO WRITE IT (GENERIC DO THIS)
+
 const result = mergeObjects<
   { name: string; hobby: Array<string> },
   { age: number }
 >({ name: "Artem", hobby: ["Sport"] }, { age: 61 });
 
-console.log(result.name);
+// const result = mergeObjects({ name: "Artem", hobby: ["Sport"] }, 61); doesn't work because generic inherit type object
+
+console.log(result);
