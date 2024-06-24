647da4fbbcb5f55e3415b906b1c40bca83e61301
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-06-24 12:02:13 +0300

    decorator factory

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
 // decorators starts work when class is defined
 // Decorator in libraries and resources start with capital letter
-function Logger(constructor: Function) {
-  console.log("Logger at work");
-  console.log(constructor);
+function Logger(descriptionString: string) {
+  // Decorator factory(can take params and work with them)
+  return function (constructor: Function) {
+    console.log(descriptionString);
+    console.log(constructor);
+  };
 }
-@Logger
+
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const person = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = person.name;
+    }
+  };
+}
+// @Logger("LOGGING - PERSON")
+@WithTemplate("<h1>This is template</h1>", "app")
 class Person {
   name = "Artem";
 
